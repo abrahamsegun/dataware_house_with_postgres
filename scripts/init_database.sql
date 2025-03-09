b43530fbfbda6cfe0536772e918cf45a480db194
OLOWU ABRAHAM <missionaryabra@gmail.com>
2025-03-10 00:18:55 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,7 +3,7 @@
 DATAWARE HOUSE WAS CREATED WITH POSTGRESQL
 ==========================================
 
-There is a created database called "wh1-project" -- which means warehouse1_project,
+There is a created database called "wh1_project" -- which means warehouse1_project,
 warning --> This script runs the database and create the schema, it does not replace 
 anything perse but proceed with caution  
 
